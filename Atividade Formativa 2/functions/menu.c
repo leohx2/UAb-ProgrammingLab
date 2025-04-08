d8895e61a0c62929b2896cd15439a3ed333c9f40
leohx2 <leobgse@gmail.com>
2025-04-09 00:14:46 +0100

    Trying to config it for windows in a pc without gcc

---
+++ Atividade Formativa 2/functions/menu.c
@@ -0,0 +1,152 @@
+#include "../header/catalog.h"
+#include "../header/helper.h"
+#include "../header/database.h"
+#include <stdlib.h>
+
+// The prototype functions below will be used here, and only in this file, that's why
+// it's not in the header file with the remaining functions.
+void Edit_menu(LCatalog *l_catalog);
+void Search_menu(LCatalog *l_catalog);
+
+LCatalog *Initial_menu(TLists *t_listas, char *file_name, TFiles *t_files, char *username)
+{
+  int user_choice = 0, id;
+
+  while (user_choice != -1)
+  {
+    printf("\nPlease choose one of the options below\n 1 - Choose a show\n 2 - Display all shows\n 3 - Search\n 4 - Add a new show");
+    printf("\n 5 - Remove a show\n 6 - Edit a show\n 7 - Save\n 8 - See my playlists\n 9 - Create a new playlist\n10 - Delete a playlist\n11 - StreamFlix recommendation\n-1 - Exit\n");
+    printf("Option: ");
+    user_choice = Safe_answer();
+
+    switch (user_choice)
+    {
+    case -1:
+    {
+      printf("\nThank you for using StreamFlix :)\n");
+      break;
+    }
+    case 1:
+    {
+      t_listas->l_interactions = Watch_a_movie(t_listas, t_files, username);
+      break;
+    }
+    case 2:
+    {
+      printf("\nHere's a list of all the titles we have:\n");
+      Print_catalog(t_listas->l_catalog);
+      break;
+    }
+    case 3:
+    {
+      Search_menu(t_listas->l_catalog);
+      break;
+    }
+    case 4:
+    {
+      t_listas->l_catalog = User_add_new_title(t_listas->l_catalog);
+      break;
+    }
+    case 5:
+    {
+      printf("\nID to remove: ");
+      id = Safe_answer();
+      t_listas->l_catalog = User_remove_title(t_listas->l_catalog, id);
+      break;
+    }
+    case 6:
+    {
+      Edit_menu(t_listas->l_catalog);
+      break;
+    }
+    case 7:
+    {
+      t_files->movies = Save_Catalog(t_listas->l_catalog, t_files->movies, file_name);
+      printf("\n Changes saved!\n");
+      break;
+    }
+    case 8:
+    {
+      Show_playlist_titles(t_listas, t_files, username);
+      break;
+    }
+    case 9:
+    {
+      t_listas->l_playlist = Create_new_favorite(t_listas->l_playlist, t_files->favorites);
+      break;
+    }
+    case 10:
+    {
+      Delete_playlist(t_listas, t_files, username);
+      break;
+    }
+    case 11:
+    {
+      // To make sure the interactions file is updated we'll close and reopen it.
+      fclose(t_files->interactions);
+      Recommendation(t_listas->l_catalog, username);
+#ifdef _WIN32
+      t_files->interactions = Open_interaction_csv(".\\database_csv\\interaction_", username, "r+");
+#else
+      t_files->interactions = Open_interaction_csv("./database_csv/interaction_", username, "r+");
+#endif
+      break;
+    }
+    default:
+      printf("Invalid option\n");
+    }
+  }
+  return t_listas->l_catalog;
+}
+
+void Edit_menu(LCatalog *l_catalog)
+{
+  int id, edit_option;
+
+  printf("\nTitle ID to edit: ");
+  id = Safe_answer();
+  printf("\nPlease, choose one of the options below to edit\n");
+  printf("1 - Title.\n2 - Category.\n3 - Duration.\n4 - PEGI.\n5 - Views.\n6 - Everything.\n0 - Previous menu.\n");
+  printf("Option: ");
+  edit_option = Safe_answer();
+  if (edit_option == 0)
+    return;
+  if (edit_option > 6 || edit_option < 1)
+    printf("Invalid option\n");
+  if (Edit_item(l_catalog, edit_option, id) == 0)
+    printf("ID not found\n");
+}
+
+void Search_menu(LCatalog *l_catalog)
+{
+  int user_choice;
+
+  printf("\nChoose one of the options below:\n");
+  printf("Search by\n1 - Title\n2 - Category\n3 - Age Rating\n");
+  printf("option:");
+  user_choice = Safe_answer();
+
+  if (user_choice == 1 || user_choice == 2)
+    Search_title_or_category(l_catalog, user_choice);
+  else if (user_choice == 3)
+    Search_PEGI(l_catalog);
+  else
+    printf("Invalid option.\n");
+}
+
+int Remove_from_playlist_menu()
+{
+  int user_choice = -2;
+
+  while (user_choice < -1 || user_choice == 0)
+  {
+    printf("\nIf you want to remove a title from a playlist, type the title ID, otherwise, -1 to return to main menu\n");
+    printf("Option: ");
+    user_choice = Safe_answer();
+
+    if (user_choice == 0)
+      printf("\nInvalid option.\n");
+  }
+
+  return user_choice;
+}
---
+++ Atividade Formativa 2/main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Include the lists to use and it functions as well during the program.
+#include "header/listas.h"
+// Include the auxiliar functions to use in main
+#include "header/catalog.h"
+#include "header/helper.h"
+#include "header/database.h"
+
+#define FILENAME "movies_data.csv"
+
+int main()
+{
+  FILE *f_user;
+  TFiles *t_files;
+  TLists *t_lists;
+  TUser *t_user;
+  int user_choice = 0;
+
+  t_lists = (TLists *)malloc(sizeof(TLists));
+  t_files = (TFiles *)malloc(sizeof(TFiles));
+  if (t_lists == NULL || t_files == NULL)
+  {
+    printf("ERROR, memory allocation.\n");
+    return EXIT_FAILURE;
+  }
+
+  t_lists->l_catalog = NULL;
+  f_user = NULL;
+
+  // To work properly, the CSV file must have the follwing sintax:
+  // Title, Category, Duration (min), Recommended classification age, View.
+  t_files->movies = fopen("movies_data.csv", "r");
+#ifdef _WIN32
+  f_user = fopen(".\\database_csv\\user.csv", "r+");
+#else
+  f_user = fopen("./database_csv/user.csv", "r+");
+#endif
+
+  if (t_files->movies == NULL)
+  {
+    printf("Error. File 'movies_data.csv' not found.\n");
+    return EXIT_FAILURE;
+  }
+  if (f_user == NULL)
+  {
+    printf("Error. user.csv not found.\n");
+    return EXIT_FAILURE;
+  }
+
+  t_lists->l_catalog = Save_on_list(t_files->movies, t_lists->l_catalog);
+
+  while (user_choice != 2)
+  {
+    printf("\n------------- Please choose an option -------------\n");
+    printf("1: StreamFlix report\n2: Login\n");
+    printf("option: ");
+    user_choice = Safe_answer();
+    if (user_choice == 1)
+      Report(t_lists->l_catalog);
+    else if (user_choice == 2)
+      t_user = Login(f_user);
+    else
+      printf("\nInvalid option\n");
+  }
+
+  fclose(f_user);
+  printf("\n____________Welcome to Streamflix, %s!____________\n", t_user->username);
+
+// Load the user interactions in the list
+#ifdef _WIN32
+  t_files->interactions = Open_interaction_csv(".\\database_csv\\interaction_", t_user->username, "r+");
+#else
+  t_files->interactions = Open_interaction_csv("./database_csv/interaction_", t_user->username, "r+");
+#endif
+  if (t_files->interactions == NULL)
+  {
+    printf("Error, user database not found, interactions.\n");
+    fclose(t_files->movies);
+    return EXIT_FAILURE;
+  }
+
+#ifdef _WIN32
+  t_files->favorites = Open_interaction_csv(".\\database_csv\\lists_", t_user->username, "r+");
+#else
+  t_files->favorites = Open_interaction_csv("./database_csv/lists_", t_user->username, "r+");
+#endif
+  if (t_files->favorites == NULL)
+  {
+    printf("Error, user database not found, favorites.\n");
+    fclose(t_files->movies);
+    fclose(t_files->interactions);
+    return EXIT_FAILURE;
+  }
+
+  t_lists->l_playlist = Load_favorites(t_files->favorites);
+
+  t_lists->l_interactions = Load_Interactions(t_files->interactions);
+
+  t_lists->l_catalog = Initial_menu(t_lists, FILENAME, t_files, t_user->username);
+
+  t_files->movies = Save_Catalog(t_lists->l_catalog, t_files->movies, FILENAME);
+
+  t_lists->l_catalog = Free_all_lists(t_lists->l_catalog);
+  t_user = Free_user(t_user);
+  t_lists->l_interactions = Free_all_interactions(t_lists->l_interactions);
+  t_lists->l_playlist = Free_all_playlists(t_lists->l_playlist);
+
+  fclose(t_files->movies);
+  fclose(t_files->interactions);
+  fclose(t_files->favorites);
+
+  free(t_files);
+  free(t_lists);
+}
